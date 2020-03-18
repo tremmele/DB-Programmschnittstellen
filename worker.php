b85c5813a62cd7d7f5e8ceb922dd31572601f5de
Rainer Laengst <rainer.laengst@web.de>
2020-03-18 20:32:53 +0100

    add basic worker

--- worker.php
+++ worker.php
@@ -1,5 +1,29 @@
 <?php
-  include crawl.php;
+  include "crawl.php";
   
+  $dbUrl = "127.0.0.1";
+  $dbUser = "root";
+  $dbPassword = "";
+  $dbName = "mydb";
+
+
+  //while(true)
+  //{
+    // Connect to DB
+    $mysqli = new mysqli($dbUrl, $dbUser, $dbPassword, $dbName);
+    if ($mysqli->connect_errno)
+    {
+        echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+
+    // Get URLs From DB
+    $result = $mysqli->query("SELECT link FROM site");
+    while ($row = $result->fetch_assoc()) 
+    {
+        // Print every URL
+        echo "URL: " . $row['link'] . "\n";
+    }
+
+  //}
 
 ?>
