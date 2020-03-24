3b161caedd7a2742098ba78369e4de1042cb8ba3
R-Laengst <rainer.laengst@web.de>
2020-03-24 15:20:27 +0100

    add search form and function

--- frontend/index.php
+++ frontend/index.php
@@ -8,15 +8,17 @@
     <?php
         //includes
         include "functions/addUrl.php";
+        include "functions/search.php";
 
 
-        // Add URL to DB 
+        // Add URL Form 
         echo "<form action=\"\" method=\"post\">";
             echo "<label for=\"url\">URL:</label>";
             echo "<input type=\"text\" id=\"url\" name=\"url\"><br><br>";
             echo "<input type=\"submit\" value=\"hinzufügen\">";
         echo "</form>";
 
+        // Add URL to DB
         if(isset($_POST['url']))
         {
             // Add URL
@@ -24,10 +26,20 @@
             echo "Die URL " . $_POST['url'] . " wurde hinzugefügt";
         }
 
-        // Search window
+        // Search Form
+        echo "<form action=\"\" method=\"post\">";
+            echo "<label for=\"searchQuery\">Suchanfrage:</label>";
+            echo "<input type=\"text\" id=\"searchQuery\" name=\"searchQuery\"><br><br>";
+            echo "<input type=\"submit\" value=\"suchen\">";
+        echo "</form>";
         
         // Print search results  
-
+        if(isset($_POST['searchQuery']))
+        {
+            // Add URL
+            search($_POST['searchQuery']);
+            echo "Suchanfrage: " . $_POST['searchQuery'];
+        }
     ?>
     </body>
 </html>
