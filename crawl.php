c0e1d5633a690fd103c77bbda3cbee8500e21939
Timo Remmele <remmele.timo@web.de>
2020-03-24 14:51:14 +0100

    MAINT: added sql insert statement

--- crawl.php
+++ crawl.php
@@ -23,7 +23,7 @@
 
     protected function _get_images() {
       if (!empty($this->markup)){
-        preg_match_all('/<img([^>]+)\/>/i', $this->markup$images); 
+        preg_match_all('/<img([^>]+)\/>/i', $this->markup, $images); 
         return !empty($images[1]) ? $images[1] : FALSE;
       }
     }
@@ -43,11 +43,39 @@
 <h2>Webcrawler</h2>
 <?php
 
-foreach($links as $l) {
-  if (substr($l,0,7)!='http://')
-    echo "<br>Link: $crawl->base/$l";
-}
+$dbUrl = "127.0.0.1";
+$dbUser = "root";
+$dbPassword = "";
+$dbName = "mydb";
+$crawlInterval = 1000;//86400;
 
+//while(true)
+//{
+  // Connect to DB
+  $mysqli = new mysqli($dbUrl, $dbUser, $dbPassword, $dbName);
+  if ($mysqli->connect_errno)
+  {
+      echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+  }
+
+#foreach($links as $l) {
+#  if (substr($l,0,7)!='http://')
+#    echo "<br>Link: $crawl->base/$l";
+#}
+
+$crawl = new Crawler('http://www.heidenheim.dhbw.de');
+#echo implode($crawl -> get("links"));
+
+foreach($crawl -> get("links") as $link) {
+  
+  #$sql = "INSERT INTO site (link, timestamp) VALUES ($link, date('Y-m-d H:i:s'))";
+  $timestamp = date('Y-m-d H:i:s');
+  $insert_stmt = $mysqli->prepare("INSERT INTO site (link, time_stamp) VALUES (?,?)");
+  $insert_stmt->bind_param('ss', $link, $timestamp);
+  $insert_stmt->execute();
+
+}
+$mysqli->close();
 ?>
 </body>
 </html>
